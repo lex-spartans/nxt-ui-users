ca98eaf35c2f4125af475975db13e7714fd63e55
Servando Salazar Valle <servando.salazar@spaymentservices.com>
2024-09-10 11:58:35 -0600

    rebase to develop and main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { HierarchyComponent } from './pages/hierarchy/hierarchy.component';
 
-export const routes: Routes = [
-  {
-    path: '',
-    component: HierarchyComponent,
-  },
-];
+export const routes: Routes = [];
