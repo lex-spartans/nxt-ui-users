26d92b7162221c5fb564bf19280e1b86da009712
Alexis Ayala Solorio <alexis.ayala@spaymentservices.com>
2024-11-27 17:29:40 -0600

    feat(i18n): add translation support for multiple languages 🎉
    - Added support for translation using `@ngx-translate/core` and `@ngx-translate/http-loader`.
    - Integrated translation feature in `app.component.html` to display welcome message in different languages.
    - Added translation files for English (`en.json`) and Spanish (`es.json`).
    - Implemented language service to manage and switch between languages.
    - Modified `app.component.ts` and `app.component.spec.ts` to support language switching and translations.
    - Updated configuration to load translation files dynamically.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,24 @@
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  HttpClient,
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 import { environment } from '../environments/environment';
 import { IEnvironments } from '../interfaces/iEnvironments';
 
 import { routes } from './app.routes';
-import { authInterceptorInterceptor } from './shared/interceptors/auth-interceptor.interceptor';
+import { HttpLoaderFactory } from './app.translation';
+import { authInterceptor } from './shared/interceptors/auth-interceptor.interceptor';
 import { ENV_CONFIG } from './shared/tokens/environments-config';
 
 /**
@@ -21,9 +32,19 @@ export const appConfig: ApplicationConfig = {
       provide: ENV_CONFIG,
       useValue: environment as unknown as IEnvironments,
     },
-    provideHttpClient(withInterceptors([authInterceptorInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor]), withFetch()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+        defaultLanguage: 'es',
+      }),
+    ),
   ],
 };
